c7c06cf56a182e224ca4c143143977f9a2a334f6
pkr <rajarajeshwari9344534@gmail.com>
2026-03-19 19:01:52 +0530

    fix: origin

--- main.py
+++ main.py
@@ -39,12 +39,7 @@ def root():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://127.0.0.1:5500",
-        "http://localhost:5500",
-        "https://justicenest.netlify.app",
-        "https://www.justicenest.netlify.app"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
